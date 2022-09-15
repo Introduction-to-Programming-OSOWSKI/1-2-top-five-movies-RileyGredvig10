765ba4d69a0e7829967e7e675d42694aa485411a
RileyGredvig10 <821118@mystma.org>
2022-09-15 09:45:58 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+print (topFiveMovies("Top Gun ", "Indiana Jones ", "Star Wars: The Return of The Jedi ", "Ghostbusters ", "Back to The Future "))
